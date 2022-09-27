68adbd08e74fe49ca12b8e2270d26fe5f4ff9c1c
Mario Conti <marioconti91@gmail.com>
2022-09-27 01:14:00 -0300

    prueba

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -7,25 +7,36 @@ import "./styles.css";
 export const Home = () => {
   return (
     <div id="/" className="home-container">
-      <div className="mask">
-    
-        <h1 className="title-home" style={{userSelect:"none",marginRight:8}}>
-          Guada  <span className="letter-home-title"> <span style={{fontFamily: 'Oooh Baby', fontSize:"0.8em", marginRight:0}}> &  </span> </span> Santi
-        </h1>
-        <h2 className="sub-title-home" style={{userSelect:"none"}}>¡NOS CASAMOS!</h2>
-        {/* TODO:AQUI HAY QUE HACER EN DESKTOP POSITION PARA MANEJAR LOS ELEMENTOS ASI NO TAPAN CARAS */}
-        {/* <CountDownMarried /> */}
-        {/* <Link
-          className="link-confirmation"
-          to="confirmation"
-          spy={true}
-          smooth={true}
-          offset={0}
-          duration={1000}
-        >
-          CONFIRMAR ASISTENCIA
-        </Link> */}
-      </div>
+      {/* <div className="mask">
+        <div className="container-names" style={{ position: "relative" }}>
+        <h2 className="sub-title-home subtitle-guada-y-fla" style={{userSelect:"none"}}>¡NOS CASAMOS!</h2> 
+          <div className="nombre-guada">
+            <div className="ubicacion-nombres-guada" />
+            <div style={{ color: "#ebebeb", margin: 0 }} className="guada">
+              <p style={{ margin: 0 }}>Guada</p>
+            </div>
+            <div style={{ color: "#ebebeba6" }} className="container-avila">
+              <p className="avila">Avila</p>
+            </div>
+          </div>
+
+          <div>
+            <div className="letra-y" style={{ divosition: "absolute" }}>
+              &
+            </div>
+          </div>
+          <div className="nombre-fla">
+            <div className="ubicacion-nombres-fla">
+              <div style={{ color: "#ebebeb"}} className="santi-container">
+                <p  className="santi">Santi</p>
+              </div>
+              <div style={{ color: "#ebebeb83" }} className="container-fla">
+                <p className="fla">Flass</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 };
